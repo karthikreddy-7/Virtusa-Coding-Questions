d207e91ba10bda78f5a3975869668c22c5b66810
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-12-08 14:23:21 +0530

    palindrome

--- Basics/Palindrome.java
+++ Basics/Palindrome.java
@@ -1,5 +1,29 @@
 package Basics;
 
-public class Palindrome {
+import java.util.Scanner;
 
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the word to check for palindrome: ");
+        String s = sc.nextLine();
+        int start = 0;
+        int end = s.length() - 1;
+        Boolean flag = false;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                flag = true;
+                break;
+            } else {
+                start++;
+                end--;
+            }
+        }
+        if (!flag) {
+            System.out.println("the word " + s + " is a palindrome.");
+        } else {
+            System.out.println("it is not a palindrome");
+        }
+        sc.close();
+    }
 }
