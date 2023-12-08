9828d038c2a29dd67367b62b18ac7a5057f40321
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-12-08 19:18:59 +0530

    maximumsumsubarray

--- Basics/MaximumSumSubarray.java
+++ Basics/MaximumSumSubarray.java
@@ -1,5 +1,40 @@
 package Basics;
 
-public class MaximumSumSubarray {
+import java.util.Scanner;
 
+public class MaximumSumSubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length of the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + " element : ");
+            arr[i] = sc.nextInt();
+        }
+        // finding maximum sum in subarray: Brute Force
+        int maximum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int cursum = 0;
+            for (int j = i; j < n; j++) {
+                cursum += arr[j];
+                if (cursum > maximum) {
+                    maximum = cursum;
+                }
+            }
+        }
+        System.out.println(maximum);
+        int maxsum = 0;
+        int cursum = 0;
+        for (int i = 0; i < n; i++) {
+            cursum += arr[i];
+            if (cursum > maxsum) {
+                maxsum = cursum;
+            }
+            if (cursum < 0) {
+                cursum = 0;
+            }
+        }
+        System.out.println(maxsum);
+    }
 }
