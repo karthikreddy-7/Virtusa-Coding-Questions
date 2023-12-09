f8dcce142abc103665aac0b0321a8a9280c9921c
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-12-09 10:39:41 +0530

    Ith bit

---
+++ Coding_QandA/FindingIthBit.java
@@ -0,0 +1,17 @@
+package Coding_QandA;
+
+import java.util.Scanner;
+
+//find the Ith Bit of a number whether it is 0/1
+
+public class FindingIthBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of N:");
+        int n = sc.nextInt();
+        System.out.println("enter the Ith value:");
+        int i = sc.nextInt();
+        int mask = 1 << i;
+        System.out.println("the Ith bit is " + ((n & mask) != 0 ? 1 : 0));
+    }
+}
